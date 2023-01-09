3912f2d5b51fdff189e85afb2c05d7d28ef9fbe1
Danil Bohutskiy <bohutskiy2@gmail.com>
2023-01-10 01:20:38 +0200

    init project

---
+++ main.py
@@ -0,0 +1,48 @@
+import os, pathlib, shutil, PIL
+from PIL import Image
+
+WORKDIR = os.getcwd()
+OUTPUTDIR = '\\output\\'
+
+def getImagePil(filename):
+    img = PIL.Image.open(filename)
+    
+    myHeight, myWidth = img.size
+    img.resize((myHeight, myWidth), Image.Resampling.LANCZOS)
+    
+    return img
+
+def refreshDirectory(path):
+    dirpath = os.path.join((WORKDIR + OUTPUTDIR), path)
+    if os.path.isdir(dirpath):
+        os.chmod(dirpath, 0o0777)
+        shutil.rmtree(dirpath)
+    else:
+        os.mkdir(dirpath)
+
+def saveToDirectory(img, index, filename, path):
+    dirpath = os.path.join((WORKDIR + OUTPUTDIR), path)
+
+    file_extension = pathlib.Path(filename).suffix
+    new_filename = str(index) + file_extension
+    img.save(os.path.join(dirpath, new_filename))
+
+    print('File \"' + filename + '\" saved as \"' + new_filename + '\"')
+
+def run():
+    print('Start...')
+
+    os.chdir('./src')
+
+    for directory in os.listdir():
+        os.chdir('./' + directory)
+        print('Current directory: ' + os.getcwd())
+        refreshDirectory(directory)
+        for index, filename in enumerate(os.listdir()):
+            img = getImagePil(filename)
+            saveToDirectory(img, (index + 1), filename, directory)
+        os.chdir('..')
+    
+    print('Done!')
+
+run()
