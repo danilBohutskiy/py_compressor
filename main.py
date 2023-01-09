6761ed003eebd6ed304f80f31de8c3950e82b5cc
Danil Bohutskiy <bohutskiy2@gmail.com>
2023-01-10 01:32:36 +0200

    update folders

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os, pathlib, shutil, PIL
 from PIL import Image
 
 WORKDIR = os.getcwd()
-OUTPUTDIR = '\\output\\'
+INPUTDIR = 'src'
+OUTPUTDIR = 'output'
 
 def getImagePil(filename):
     img = PIL.Image.open(filename)
@@ -13,7 +14,7 @@ def getImagePil(filename):
     return img
 
 def refreshDirectory(path):
-    dirpath = os.path.join((WORKDIR + OUTPUTDIR), path)
+    dirpath = os.path.join((WORKDIR + '\\' + OUTPUTDIR), path)
     if os.path.isdir(dirpath):
         os.chmod(dirpath, 0o0777)
         shutil.rmtree(dirpath)
@@ -21,7 +22,7 @@ def refreshDirectory(path):
         os.mkdir(dirpath)
 
 def saveToDirectory(img, index, filename, path):
-    dirpath = os.path.join((WORKDIR + OUTPUTDIR), path)
+    dirpath = os.path.join((WORKDIR + '\\' + OUTPUTDIR), path)
 
     file_extension = pathlib.Path(filename).suffix
     new_filename = str(index) + file_extension
@@ -29,10 +30,23 @@ def saveToDirectory(img, index, filename, path):
 
     print('File \"' + filename + '\" saved as \"' + new_filename + '\"')
 
+def initDirectories():
+    outputpath = os.path.join(WORKDIR, INPUTDIR)
+    if os.path.isdir(outputpath):
+        os.chmod(outputpath, 0o0777)
+    else:
+        os.mkdir(outputpath)
+    inputpath = os.path.join(WORKDIR, OUTPUTDIR)
+    if os.path.isdir(inputpath):
+        os.chmod(inputpath, 0o0777)
+    else:
+        os.mkdir(inputpath)
+
+
 def run():
     print('Start...')
-
-    os.chdir('./src')
+    initDirectories()
+    os.chdir('./' + INPUTDIR)
 
     for directory in os.listdir():
         os.chdir('./' + directory)
