e25d5ca13b0ed342413def9489c35dc74242813e
Danil Bohutskiy <bohutskiy2@gmail.com>
2023-01-11 03:26:42 +0200

    Update VideoCompressor, fix bug

--- modules/VideoCompressor.py
+++ modules/VideoCompressor.py
@@ -1,13 +1,12 @@
-import mimetypes, os
+import os
 import subprocess
 
 class VideoCompressor:
-    
     def compress(self, path_to_file, path_to_output, filename):
         filename_ext = os.path.splitext(filename)
         filename_new = filename_ext[0] + '.mp4'
+        filename_new = filename_new.replace(" ", "_")
         file_path = os.path.join(path_to_output, filename_new)
-        
-        result = subprocess.run('ffmpeg -i '+path_to_file+' -vcodec libx265 -crf 28 '+file_path)
-        print(result)
-        
+
+        cmd = f'ffmpeg -i "{path_to_file}" -vcodec libx265 -crf 28 "{file_path}"'
+        result = subprocess.Popen(cmd, shell=True)
